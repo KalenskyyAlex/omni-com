ed7a62a5f65b7f02944d4e03f74b161d447f0dc4
Oleksandr Kalenskyy <oleksandr.kalenskyy@student.tuke.sk>
2024-08-11 22:14:13 +0200

    tab names stored dynamically

--- src/Codespace/Codespace.tsx
+++ src/Codespace/Codespace.tsx
@@ -1,16 +1,47 @@
+import {useState} from "react";
+
 import '../index.css';
 import './Codespace.css';
 
 function Codespace () {
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
+    const [tabContents, setTabContent] = useState([""]);
+    const [tabNames, setTabNames] = useState([[0, "script1.min"]])
+
+    const switchTabs = (index: number) => {
+        setActiveTabIndex(index);
+    }
+
     return (
         <div className="code-space">
             <div className="horizontal-group underline-group code-border">
                 <div className="horizontal-group">
-                    <button id="examples" className="text-button code-primary-text tab-primary">
-                        script1.min
-                    </button>
-                    <button id="examples" className="text-button code-secondary-text tab-secondary">
-                        script2.min
+                    {
+                        tabNames.map((pair) => {
+                            const index = pair[0];
+                            const tabName = pair[1];
+                            if (index === activeTabIndex) {
+                                return (
+                                    <button id="examples"
+                                            className="text-button code-primary-text tab-primary horizontal-group center">
+                                        {tabName}
+                                        <div className="close-tab-primary-icon"></div>
+                                    </button>
+                                )
+                            } else {
+                                return (
+                                    <button id="examples"
+                                            className="text-button code-secondary-text tab-secondary horizontal-group center">
+                                        {tabName}
+                                        <div className="close-tab-secondary-icon"></div>
+                                    </button>
+                                )
+                            }
+                        })
+                    }
+                    <button id="examples" className="text-button code-secondary-text tab-secondary horizontal-group center">
+                        new
+                        <div className="add-tab-icon"></div>
                     </button>
                     {/*Todo*/}
                 </div>
@@ -19,7 +50,7 @@ function Codespace () {
                         <div className="upload-icon"></div>
                     </button>
                     <button id="download" className="icon-button tab-secondary">
-                        <div className="download-icon"></div>
+                    <div className="download-icon"></div>
                     </button>
                     <button id="examples" className="text-button code-secondary-text tab-secondary">
                         Examples
