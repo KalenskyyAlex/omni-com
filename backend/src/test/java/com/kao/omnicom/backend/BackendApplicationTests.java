656aa8c31cb26e6ad75c8c2900d627e2d8dc64e5
Oleksandr Kalenskyy <oleksandr.kalenskyy@student.tuke.sk>
2024-08-19 22:57:26 +0200

    backend innit

---
+++ backend/src/test/java/com/kao/omnicom/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kao.omnicom.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
