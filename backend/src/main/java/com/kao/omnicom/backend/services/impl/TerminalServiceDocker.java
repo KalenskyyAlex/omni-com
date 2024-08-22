38cd0ed0fc6f3c471ae41c2d166d1228f464054e
Oleksandr Kalenskyy <oleksandr.kalenskyy@student.tuke.sk>
2024-08-23 00:45:48 +0200

    file output retrieval

--- backend/src/main/java/com/kao/omnicom/backend/services/impl/TerminalServiceDocker.java
+++ backend/src/main/java/com/kao/omnicom/backend/services/impl/TerminalServiceDocker.java
@@ -2,7 +2,6 @@ package com.kao.omnicom.backend.services.impl;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.command.WaitContainerResultCallback;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
@@ -10,8 +9,15 @@ import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
 
 import com.kao.omnicom.backend.services.TerminalService;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.aspectj.util.FileUtil;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class TerminalServiceDocker implements TerminalService {
     private final DockerClientConfig defaultConfig = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
@@ -25,60 +31,51 @@ public class TerminalServiceDocker implements TerminalService {
     public void getOutput(File input) {
         DockerClient client = DockerClientImpl.getInstance(defaultConfig, httpClient);
 
-        CreateContainerResponse container = client.createContainerCmd("minimum:1.0.2")
+        CreateContainerResponse container = client.createContainerCmd("minimum:1.0.3")
                 .withAttachStderr(true)
                 .withAttachStdout(true)
                 .withTty(true)
-                .withEnv("FLAGS=-c -l -p")
+//                .withEnv("FLAGS=-c -l -p")
                 .exec();
-
-
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
         client.startContainerCmd(container.getId()).exec();
 
-        WaitContainerResultCallback resultCallback = new WaitContainerResultCallback();
-        client.waitContainerCmd(container.getId()).exec(resultCallback);
+        Thread t = new Thread(() -> {
+            while (client.inspectContainerCmd(container.getId()).exec().getState().getRunning()) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+            try {
+                InputStream archive = client.copyArchiveFromContainerCmd(container.getId(), "/omnicom/output.txt").exec();
+
+                TarArchiveInputStream tar = new TarArchiveInputStream(archive);
+                TarArchiveEntry currentEntry = tar.getNextTarEntry();
+                BufferedReader br = null;
+                StringBuilder sb = new StringBuilder();
+                while (currentEntry != null) {
+                    br = new BufferedReader(new InputStreamReader(tar)); // Read directly from tarInput
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                    currentEntry = tar.getNextTarEntry(); // You forgot to iterate to the next file
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                throw new RuntimeException(e);
+            }
+        });
+
+        t.start();
+
         try {
-            resultCallback.awaitCompletion();
+            t.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        LogContainerCmd logContainerCmd = client.logContainerCmd(container.getId());
-//        logContainerCmd.withStdOut(true).withStdErr(true);
-//
-//        logContainerCmd.withTimestamps(true);
-//
-//        try {
-//            logContainerCmd.exec(new LogContainerResultCallback() {
-//                @Override
-//                public void onNext(Frame item) {
-//                    System.out.println(item);
-//                }
-//            }).awaitCompletion();
-//        } catch (InterruptedException e) {
-//            System.out.println(":(");
-//        }
-
-//        try {
-//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//
-//            client.execStartCmd(container.getId())
-//                    .exec(new ExecStartResultCallback(outputStream, outputStream))
-//                    .awaitCompletion();
-//
-//            System.out.println(outputStream.toString());
-//
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
     }
 }
