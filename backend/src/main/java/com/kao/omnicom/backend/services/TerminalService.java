ec722592fb19169860d825837f4fbe72ececde98
Oleksandr Kalenskyy <oleksandr.kalenskyy@student.tuke.sk>
2024-08-24 15:53:52 +0200

    updated .gitingore

--- backend/src/main/java/com/kao/omnicom/backend/services/TerminalService.java
+++ backend/src/main/java/com/kao/omnicom/backend/services/TerminalService.java
@@ -2,8 +2,6 @@ package com.kao.omnicom.backend.services;
 
 import com.kao.omnicom.backend.entity.OutputResponse;
 
-import java.io.File;
-
 public interface TerminalService {
 
     OutputResponse getOutput(byte[] input, String flags);
