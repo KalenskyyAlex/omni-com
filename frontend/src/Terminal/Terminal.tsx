c8245eef7a724c0e7bdd4ec61c77a08e54466564
Oleksandr Kalenskyy <oleksandr.kalenskyy@student.tuke.sk>
2024-09-03 23:31:32 +0200

    terminal api calls refactor

--- frontend/src/Terminal/Terminal.tsx
+++ frontend/src/Terminal/Terminal.tsx
@@ -12,32 +12,63 @@ interface APIResponse {
     "waitingForInput": boolean;
 }
 
+const defaultResponse: APIResponse = {
+    output: "If you see this message, backend server is\n" +
+        "probably stopped, or ran into critical problem.\n" +
+        "Contact me at kalenskyy.oleksandr@gmail.com.\n" +
+        "\n" +
+        "If you are testing in local environment,\n" +
+        "make sure server is running on localhost:8080",
+    containerId: "",
+    waitingForInput: false
+};
+
+function init(code: string) : APIResponse {
+    let result: APIResponse = defaultResponse;
+
+    const api_root = process.env["REACT_APP_OMNICOM_API_LOCAL"];
+    fetch(api_root + "/terminal/init", {
+        method: "POST",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Content-Type": "application/json",
+            "Cache-Control": "no-cache",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Max-Age": "3600"
+        },
+        body: JSON.stringify({
+            "code": code
+        })
+    })
+        .then((response: Response) => {
+            response.json()
+                .then((apiResponse: APIResponse) => result = apiResponse)
+        })
+        .catch(console.error);
+
+    return result;
+}
+
+function provide_input(userInput: string, containerId: string): APIResponse {
+    let result: APIResponse = defaultResponse;
+    // TODO
+    return result;
+}
+
+function terminate(containerId: string): APIResponse {
+    let result: APIResponse = defaultResponse;
+    // TODO
+    return result;
+}
+
 function Terminal(props: TerminalProps) {
     const [terminalOutput, setTerminalOutput] = useState("");
 
     const terminalInit = () => {
         const code = props.terminalCallback();
-        const api_root = process.env["REACT_APP_OMNICOM_API_LOCAL"];
-        fetch(api_root + "/terminal/init", {
-            method: "POST",
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*",
-                "Access-Control-Allow-Credentials": "true",
-                "Content-Type": "application/json",
-                "Cache-Control": "no-cache",
-                "Access-Control-Allow-Headers": "Content-Type, Authorization",
-                "Access-Control-Max-Age": "3600"
-            },
-            body: JSON.stringify({
-                "code": code
-            })
-        })
-            .then((response: Response) => {
-                response.json()
-                    .then((apiResponse: APIResponse) => setTerminalOutput(apiResponse.output))
-            })
-            .catch(console.error);
+        setTerminalOutput(init(code).output);
     }
 
     return (
