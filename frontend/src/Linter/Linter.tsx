3ee2e5ee8bdbc30ae6b0881111da3508fb4eb071
Oleksandr Kalenskyy <oleksandr.kalenskyy@student.tuke.sk>
2024-09-04 20:42:03 +0200

    fixed variables linting with regex

--- frontend/src/Linter/Linter.tsx
+++ frontend/src/Linter/Linter.tsx
@@ -14,13 +14,26 @@ function Linter(props: LinterProps) {
     const keywordRegex = /\b(start|end|use|return|break|while|if|else|elif)\b/g;
     const operatorRegex = /(\+|-|\*|\/|%|\(|\)|\bis\b|\band\b|\bnot\b|>|<|<=|>=|==|!=|\||=)/g;
     const booleanRegex = /\b(true|false)\b/g;
-    const numeralRegex = /\b\d+\b/g;
+    const numeralRegex = /\b(?<!@)\d+\b(?!@)/g;
     const typeRegex = /\b(int|float|str|bool)/g;
     const stringLiteralRegex = /"([^"\\]|\\.)*"/g;
     const functionRegex = /(([a-zA-Z0-9_]+)\s*(?=\|)|(?<=\bstart\s+)[a-zA-Z0-9_]+)/g;
     const libraryRegex = /(?<=\buse\s+)[a-zA-Z0-9_]+/g;
-    const variableRegex = /\b(?!__\w+)\w+\b/g;
+    const variableRegex = /\b[a-zA-Z_][a-zA-Z0-9_]*\b/g;
     const commentRegex = /~(?=(?:[^"]*"[^"]*")*[^"]*$).*/g;
+
+    const codes = {
+        keyword: "@001@",
+        operator: "@002@",
+        boolean: "@003@",
+        numeral: "@004@",
+        type: "@005@",
+        string: "@006@",
+        function: "@007@",
+        library: "@008@",
+        variable: "@009@",
+        comment: "@011@",
+    }
 
     const lint = (text: string, regex: RegExp, format: string) => {
         let match;
@@ -31,7 +44,7 @@ function Linter(props: LinterProps) {
         }
 
         for (let i = positions.length - 1; i >= 0; i--) {
-            text = text.slice(0, positions[i].index) + '__' + format + text.slice(positions[i].index + positions[i].match.length);
+            text = text.slice(0, positions[i].index) + format + text.slice(positions[i].index + positions[i].match.length);
         }
 
         return {text, positions};
@@ -39,76 +52,78 @@ function Linter(props: LinterProps) {
 
     let keywords, operators, booleans, numerals, types, stringLiterals, functions, libraries, variables, comments;
 
-    let result = lint(props.content, commentRegex, "comment");
+    let result = lint(props.content, commentRegex, codes.comment);
     comments = result.positions;
 
-    result = lint(result.text, stringLiteralRegex, "strings");
+    result = lint(result.text, stringLiteralRegex, codes.string);
     stringLiterals = result.positions;
 
-    result = lint(result.text, functionRegex, "function");
-    functions = result.positions;
-
-    result = lint(result.text, libraryRegex, "library");
-    libraries = result.positions;
-
-    result = lint(result.text, keywordRegex, "keyword");
-    keywords = result.positions;
-
-    result = lint(result.text, operatorRegex, "operator");
-    operators = result.positions;
-
-    result = lint(result.text, booleanRegex, "boolean");
-    booleans = result.positions;
-
-    result = lint(result.text, numeralRegex, "numeral");
+    result = lint(result.text, numeralRegex, codes.numeral);
     numerals = result.positions;
 
-    result = lint(result.text, typeRegex, "type");
+    result = lint(result.text, functionRegex, codes.function);
+    functions = result.positions;
+
+    result = lint(result.text, libraryRegex, codes.library);
+    libraries = result.positions;
+
+    result = lint(result.text, keywordRegex, codes.keyword);
+    keywords = result.positions;
+
+    result = lint(result.text, operatorRegex, codes.operator);
+    operators = result.positions;
+
+    result = lint(result.text, booleanRegex, codes.boolean);
+    booleans = result.positions;
+
+    result = lint(result.text, typeRegex, codes.type);
     types = result.positions;
 
-    result = lint(result.text, variableRegex, "variable");
+    console.log(result.text);
+
+    result = lint(result.text, variableRegex, codes.variable);
     variables = result.positions;
 
     let formattedContent = result.text;
 
     for(let i = 0; i < comments.length; i++){
-        formattedContent = formattedContent.replace("__comment", '<code class="code-comment">' + comments[i].match + '</code>');
+        formattedContent = formattedContent.replace(codes.comment, '<code class="code-comment">' + comments[i].match + '</code>');
     }
 
     for(let i = 0; i < stringLiterals.length; i++){
-        formattedContent = formattedContent.replace("__strings", '<code class="code-string">' + stringLiterals[i].match + '</code>');
+        formattedContent = formattedContent.replace(codes.string, '<code class="code-string">' + stringLiterals[i].match + '</code>');
     }
 
     for(let i = 0; i < keywords.length; i++){
-        formattedContent = formattedContent.replace("__keyword", '<code class="code-keyword">' + keywords[i].match + '</code>');
+        formattedContent = formattedContent.replace(codes.keyword, '<code class="code-keyword">' + keywords[i].match + '</code>');
     }
 
     for(let i = 0; i < operators.length; i++){
-        formattedContent = formattedContent.replace("__operator", '<code class="code-operator">' + operators[i].match + '</code>');
+        formattedContent = formattedContent.replace(codes.operator, '<code class="code-operator">' + operators[i].match + '</code>');
     }
 
     for(let i = 0; i < booleans.length; i++){
-        formattedContent = formattedContent.replace("__boolean", '<code class="code-boolean">' + booleans[i].match + '</code>');
+        formattedContent = formattedContent.replace(codes.boolean, '<code class="code-boolean">' + booleans[i].match + '</code>');
     }
 
     for(let i = 0; i < numerals.length; i++){
-        formattedContent = formattedContent.replace("__numeral", '<code class="code-numeral">' + numerals[i].match + '</code>');
+        formattedContent = formattedContent.replace(codes.numeral, '<code class="code-numeral">' + numerals[i].match + '</code>');
     }
 
     for(let i = 0; i < types.length; i++){
-        formattedContent = formattedContent.replace("__type", '<code class="code-type">' + types[i].match + '</code>');
+        formattedContent = formattedContent.replace(codes.type, '<code class="code-type">' + types[i].match + '</code>');
     }
 
     for(let i = 0; i < functions.length; i++){
-        formattedContent = formattedContent.replace("__function", '<code class="code-function">' + functions[i].match + '</code>');
+        formattedContent = formattedContent.replace(codes.function, '<code class="code-function">' + functions[i].match + '</code>');
     }
 
     for(let i = 0; i < libraries.length; i++){
-        formattedContent = formattedContent.replace("__library", '<code class="code-library">' + libraries[i].match + '</code>');
+        formattedContent = formattedContent.replace(codes.library, '<code class="code-library">' + libraries[i].match + '</code>');
     }
 
     for(let i = 0; i < variables.length; i++){
-        formattedContent = formattedContent.replace("__variable", '<code class="code-variable">' + variables[i].match + '</code>');
+        formattedContent = formattedContent.replace(codes.variable, '<code class="code-variable">' + variables[i].match + '</code>');
     }
 
     return (
