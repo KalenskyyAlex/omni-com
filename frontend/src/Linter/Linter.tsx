58affa10033638c70ec5513c4832e84b1596d845
Oleksandr Kalenskyy <oleksandr.kalenskyy@student.tuke.sk>
2024-09-12 18:46:33 +0200

    less than < and greater than > treated correctly in Linter

--- frontend/src/Linter/Linter.tsx
+++ frontend/src/Linter/Linter.tsx
@@ -7,7 +7,8 @@ interface LinterProps {
 function getRaw(content: string){
     const tagRegex = /(<code class=[^>]*>)|(<\/code>)|(<\/?pre>)/g;
 
-    return content.replaceAll(tagRegex, '').replaceAll(/<\/?br>/g, '\n');
+    return content.replaceAll(tagRegex, '').replaceAll(/<\/?br>/g, '\n').replaceAll(/&lt;/g, '<').replaceAll(/&gt;/g, '>');
+    // .replaceAll();
 }
 
 function Linter(props: LinterProps) {
