16c456387633f0ca9ba53ba978afc0d07b7f72a3
Oleksandr Kalenskyy <oleksandr.kalenskyy@student.tuke.sk>
2024-09-20 22:13:36 +0200

    preinitialized database with superadmin

---
+++ backend/src/main/resources/data/data_example.sql
@@ -0,0 +1,5 @@
+INSERT INTO users
+    (user_id, email, password, username, role_id)
+VALUES
+    ("your id", "your email", "your password", "your username", "59cb4ce0-d1a8-4e75-a33c-98be338fdb0a");
+
