05f195663d926daf20396e78f4c07d3cd45022b2
Oleksandr Kalenskyy <oleksandr.kalenskyy@student.tuke.sk>
2024-09-28 01:10:36 +0200

    sign up

--- frontend/src/components/auth/Signup/Signup.tsx
+++ frontend/src/components/auth/Signup/Signup.tsx
@@ -6,8 +6,11 @@ import {Link} from "react-router-dom";
 import UserInput from "../UserInput/UserInput";
 import {emptyError, invalidEmailError} from "../validators";
 import CreatePassword from "../CreatePassword/CreatePassword";
+import {useState} from "react";
 
 function Signup() {
+    const [signUpPressed, setSignUpPressed] = useState(false);
+
     const emailValidator = (value: string, name: string) => {
         const [error1, message1] = emptyError(value, name);
         const [error2, message2] = invalidEmailError(value, name);
@@ -26,30 +29,56 @@ function Signup() {
                 <div className="input-container-header vertical-group center hack">
                     Create New Account
                 </div>
-                <div className="vertical-group gap24">
-                    <UserInput
-                        label="Email"
-                        type="text"
-                        for="email"
-                        inputCallback={() => {}}
-                        errorCallback={emailValidator}/>
-                    <UserInput
-                        label="Username"
-                        type="text"
-                        for="username"
-                        inputCallback={() => {}}
-                        errorCallback={emptyError}/>
-                    <CreatePassword/>
-                </div>
+                {
+                    signUpPressed ?
+                        <div className="input-container-message">
+                            You will get verification email shortly after submission
+                        </div>
+                        : <div className="vertical-group gap24">
+                            <UserInput
+                                label="Email"
+                                type="text"
+                                for="email"
+                                inputCallback={() => {
+                                }}
+                                errorCallback={emailValidator}/>
+                            <UserInput
+                                label="Username"
+                                type="text"
+                                for="username"
+                                inputCallback={() => {
+                                }}
+                                errorCallback={emptyError}/>
+                            <CreatePassword/>
+                        </div>
+                }
+
             </div>
             <div className="vertical-group gap16 center">
+                {
+                    signUpPressed ?
+                        <div className="vertical-group gap16 center">
+                            <Link className="link" to="/login">Wrong email?</Link>
+                            <Link className="link" to="/login">I didn’t receive verification email</Link>
+                        </div>
+                        : null
+                }
                 <Link className="link" to="/login">Back to Login</Link>
-                <button className="login-button hack">Sign up</button>
+                {
+                    signUpPressed ? null :
+                    <button className="login-button hack" onClick={() => {
+                        setSignUpPressed(true);
+                        console.log("A");
+                    }}>
+                        Sign up
+                    </button>
+                }
+
                 <div>Or</div>
                 <button className="login-button hack horizontal-group gap8 center" style={{
                     justifyContent: "center"
                 }}>
-                    <div className="google-icon"></div>
+                <div className="google-icon"></div>
                     Log in with Google
                 </button>
             </div>
